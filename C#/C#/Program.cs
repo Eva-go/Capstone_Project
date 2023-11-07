141ea75f29a143b0a9573de86d069b8067208906
Eva-go <whdudghks22@gmail.com>
2023-11-07 18:22:25 +0900

    서버 제작중

---
+++ C#/C#/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Sockets;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
