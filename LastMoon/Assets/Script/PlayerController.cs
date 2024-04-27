0a274aa2a7c040b1c3b4b6bef5a929eb4441b2ff
DESKTOP-IHOO7NQ\PC <whdudghks22@gmail.com>
2024-04-27 15:15:15 +0900

    qjxms xptmxm

--- LastMoon/Assets/Script/PlayerController.cs
+++ LastMoon/Assets/Script/PlayerController.cs
@@ -74,8 +74,7 @@ public class PlayerController : MonoBehaviour
         float _yRotation = Input.GetAxisRaw("Mouse X");
         Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
         myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
-        Debug.Log(myRigid.rotation);
-        Debug.Log(myRigid.rotation.eulerAngles);
+        
     }
 
     private void CameraRotation()
