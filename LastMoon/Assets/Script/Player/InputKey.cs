6abce1d393406f902654fbf5571b3e4c51a9b782
DESKTOP-IHOO7NQ\PC <whdudghks22@gmail.com>
2024-04-30 23:56:48 +0900

    UI 인벤토리 버튼 내용 추가

--- LastMoon/Assets/Script/Player/InputKey.cs
+++ LastMoon/Assets/Script/Player/InputKey.cs
@@ -30,22 +30,22 @@ public class InputKey : MonoBehaviour
                     GameObject.Find("Resources").transform.GetChild(1).gameObject.SetActive(true);
                     GameObject.Find("Resources").transform.GetChild(2).gameObject.SetActive(false);
                     GameObject.Find("Resources").transform.GetChild(3).gameObject.SetActive(false);
-                    if (Inventory)
+                    if(Inventory)
                     {
-                        GameObject.Find("Tap").transform.GetChild(0).gameObject.SetActive(true);
-                        GameObject.Find("Tap").transform.GetChild(1).gameObject.SetActive(false);
-                        GameObject.Find("Tap").transform.GetChild(2).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(0).gameObject.SetActive(true);
+                        GameObject.Find("Tab").transform.GetChild(1).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(2).gameObject.SetActive(false);
                     }
                     break;
                 case 1:
                     GameObject.Find("Resources").transform.GetChild(1).gameObject.SetActive(false);
                     GameObject.Find("Resources").transform.GetChild(2).gameObject.SetActive(true);
                     GameObject.Find("Resources").transform.GetChild(3).gameObject.SetActive(false);
-                    if(Inventory)
+                    if (Inventory)
                     {
-                        GameObject.Find("Tap").transform.GetChild(0).gameObject.SetActive(false);
-                        GameObject.Find("Tap").transform.GetChild(1).gameObject.SetActive(true);
-                        GameObject.Find("Tap").transform.GetChild(2).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(0).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(1).gameObject.SetActive(true);
+                        GameObject.Find("Tab").transform.GetChild(2).gameObject.SetActive(false);
                     }
                     break;
                 case 2:
@@ -54,11 +54,10 @@ public class InputKey : MonoBehaviour
                     GameObject.Find("Resources").transform.GetChild(3).gameObject.SetActive(true);
                     if (Inventory)
                     {
-                        GameObject.Find("Tap").transform.GetChild(0).gameObject.SetActive(false);
-                        GameObject.Find("Tap").transform.GetChild(1).gameObject.SetActive(false);
-                        GameObject.Find("Tap").transform.GetChild(2).gameObject.SetActive(true);
+                        GameObject.Find("Tab").transform.GetChild(0).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(1).gameObject.SetActive(false);
+                        GameObject.Find("Tab").transform.GetChild(2).gameObject.SetActive(true);
                     }
-                    
                     break;  
             }
         }
@@ -68,12 +67,12 @@ public class InputKey : MonoBehaviour
             if (Inventory)
             {
                 GameObject.Find("Resources").transform.GetChild(0).gameObject.SetActive(true);
-                GameObject.Find("Tap").transform.GetChild(KeyTabCode).gameObject.SetActive(true);       
+                GameObject.Find("Item_Tab").transform.GetChild(0).gameObject.SetActive(true);
             }
             else
             {
                 GameObject.Find("Resources").transform.GetChild(0).gameObject.SetActive(false);
-                GameObject.Find("Tap").transform.GetChild(KeyTabCode).gameObject.SetActive(false);
+                GameObject.Find("Item_Tab").transform.GetChild(0).gameObject.SetActive(false);
             }
         }
     }
