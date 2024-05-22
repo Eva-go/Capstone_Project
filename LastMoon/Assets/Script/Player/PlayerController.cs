5410bc68e8d439e64a2be966394b8ee7fa262137
Eva-go <whdudghks22@gmail.com>
2024-05-22 18:16:38 +0900

    랜덤 매칭 및 씬 동기화

--- LastMoon/Assets/Script/Player/PlayerController.cs
+++ LastMoon/Assets/Script/Player/PlayerController.cs
@@ -58,6 +58,7 @@ public class PlayerController : MonoBehaviour
             CameraRotation();
             CharacterRotation();
             Inside();
+            //ainmator.SetBool("isMove", false);
         }
     }
     private void Inside()
@@ -101,10 +102,8 @@ public class PlayerController : MonoBehaviour
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
-        if(pv.IsMine)
-        {
-            ainmator.SetBool("isMove", true);
-        }
+
+        ainmator.SetBool("isMove", true);
     }
 
 
