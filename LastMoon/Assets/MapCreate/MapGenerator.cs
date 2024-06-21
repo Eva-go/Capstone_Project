36c2259ba0a30d4ee3233bc7b86fb9eed9629a3c
dece1225 <dece1225@naver.com>
2024-06-21 19:12:39 +0900

    디폴트 값 조정

--- LastMoon/Assets/MapCreate/MapGenerator.cs
+++ LastMoon/Assets/MapCreate/MapGenerator.cs
@@ -361,7 +361,7 @@ public class MapGenerator : MonoBehaviour
     {
         if (placementArea != null && NodePrefabs.Length > 0)
         {
-            //PlaceNodes(1000);
+            PlaceNodes(1000);
         }
         else
         {
@@ -370,7 +370,7 @@ public class MapGenerator : MonoBehaviour
 
         if (placementArea != null && buildingPrefabs.Length > 0)
         {
-            //PlaceBuildingsOnTop(); // 건물을 꼭대기 층에 배치
+            PlaceBuildingsOnTop(); // 건물을 꼭대기 층에 배치
         }
         else
         {
