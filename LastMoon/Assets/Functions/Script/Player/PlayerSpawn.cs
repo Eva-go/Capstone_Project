5d162ecf511b11fa86fa0fd9e60583c315064275
dece1225 <dece1225@naver.com>
2024-07-29 12:00:51 +0900

    플레이어 스폰 수정 중

--- LastMoon/Assets/Functions/Script/Player/PlayerSpawn.cs
+++ LastMoon/Assets/Functions/Script/Player/PlayerSpawn.cs
@@ -5,17 +5,39 @@ using Photon.Pun;
 
 public class PlayerSpawn : MonoBehaviourPunCallbacks
 {
-    public static Transform[] points;
-    public static int idx;
+    public static List<Transform> points = new List<Transform>(); // Transform 리스트로 수정
     private GameObject player;
 
     void Start()
     {
-        points = GameObject.Find("SpawnPoint").GetComponentsInChildren<Transform>(); // 스폰 포인트 가져오기
-        if (points.Length > 1)
+        // "Building" 태그가 지정된 모든 건물 오브젝트 찾기
+        GameObject[] buildings = GameObject.FindGameObjectsWithTag("APT");
+
+        // 디버그 로그: 찾은 건물 오브젝트 수 출력
+        Debug.Log("Number of buildings found: " + buildings.Length);
+
+        foreach (GameObject building in buildings)
         {
-            idx = Random.Range(1, points.Length); // 랜덤 스폰 포인트 선택
-            SpawnPlayer();
+            // 디버그 로그: 각 건물 오브젝트 이름 출력
+            Debug.Log("Building name: " + building.name);
+
+            // 건물 하위 오브젝트 중 이름이 "point"인 오브젝트 찾기
+            Transform[] childTransforms = building.GetComponentsInChildren<Transform>();
+            foreach (Transform child in childTransforms)
+            {
+                if (child.name.Equals("Point"))
+                {
+                    points.Add(child);
+                    // 디버그 로그: 찾은 point 오브젝트 이름과 위치 출력
+                    Debug.Log("Found point: " + child.name + " at position " + child.position);
+                }
+            }
+        }
+
+        if (points.Count > 0)
+        {
+            int idx = Random.Range(1, points.Count-1); // 랜덤 스폰 포인트 선택
+            SpawnPlayer(idx);
         }
         else
         {
@@ -23,10 +45,13 @@ public class PlayerSpawn : MonoBehaviourPunCallbacks
         }
     }
 
-    public void SpawnPlayer()
+    public void SpawnPlayer(int idx)
     {
         Debug.Log("Spawning player at point: " + idx);
-        player = PhotonNetwork.Instantiate("Player", points[idx].position, points[idx].rotation, 0); // 플레이어 인스턴스화
+        Debug.Log("Point position: " + points[idx].position);
+        Debug.Log("Point rotation: " + points[idx].rotation);
+
+        player = PhotonNetwork.Instantiate("Player", points[idx].position, points[idx].rotation); // 플레이어 인스턴스화
         if (player != null)
         {
             player.name = PhotonNetwork.LocalPlayer.NickName;
@@ -34,10 +59,10 @@ public class PlayerSpawn : MonoBehaviourPunCallbacks
             Transform LocalPlayer = player.transform.Find("LocalPlayer");
             Transform Tool = player.transform.Find("Player001");
             Transform T_LocalPlayerTool = player.transform.Find("ToolCamera");
-            OtherPlayer.gameObject.SetActive(false);
-            LocalPlayer.gameObject.SetActive(true);
-            Tool.gameObject.SetActive(false);
-            T_LocalPlayerTool.gameObject.SetActive(true);
+            if (OtherPlayer != null) OtherPlayer.gameObject.SetActive(false);
+            if (LocalPlayer != null) LocalPlayer.gameObject.SetActive(true);
+            if (Tool != null) Tool.gameObject.SetActive(false);
+            if (T_LocalPlayerTool != null) T_LocalPlayerTool.gameObject.SetActive(true);
 
             PhotonView photonView = player.GetComponent<PhotonView>();
             photonView.TransferOwnership(PhotonNetwork.LocalPlayer); // 플레이어 소유권 설정
