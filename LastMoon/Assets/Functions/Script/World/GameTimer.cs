5ecfea814a169929794b08f78846ef20b987405a
Eva-go <whdudghks22@gmail.com>
2024-08-09 15:46:24 +0900

    타이머 수정완료

--- LastMoon/Assets/Functions/Script/World/GameTimer.cs
+++ LastMoon/Assets/Functions/Script/World/GameTimer.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+
 public class GameTimer : MonoBehaviourPunCallbacks
 {
     public RectTransform timerImage;
     public Image timerImageFill_LT;
     public Image timerlmageFill_HT;
+
+    public GameObject Wave_Time_Active_LT;
+    public GameObject Wave_Time_Active_HT;
+
+    private bool switchingTime = false;
 
     private float totalTime;
     public float decreaseTime = 180f; // f1 누를 때 감소시킬 시간 (3분)
@@ -15,11 +21,14 @@ public class GameTimer : MonoBehaviourPunCallbacks
     private float initialPosX = 234f;
     private float finalPosX = -234f;
 
+    private Image tempImage; // Temporary image for swapping sprites
 
     void Start()
     {
+        Wave_Time_Active_LT.SetActive(true);
+        switchingTime = false;
         GameValue.RoundEnd = false;
-        totalTime = 0.5f * (GameValue.Round - 1)*60f / GameValue.MaxRound*60f + (GameValue.setMaxtime * 60f);
+        totalTime = 0.5f * (GameValue.Round - 1) * 60f / GameValue.MaxRound * 60f + (GameValue.setMaxtime * 60f);
         currentTime = totalTime;
         timerImage.localPosition = new Vector3(initialPosX, timerImage.localPosition.y, timerImage.localPosition.z);
         timerImageFill_LT.fillAmount = 1;
@@ -27,8 +36,7 @@ public class GameTimer : MonoBehaviourPunCallbacks
         PhotonNetwork.AutomaticallySyncScene = true;
         GameValue.WaveTimer = currentTime;
         GameValue.WaveTimerMax = totalTime;
-
-        Debug.Log("토탈 타이머: " + totalTime+"현재 라운드: "+GameValue.Round+"최대 라운드: "+GameValue.MaxRound+"세팅 시간: "+GameValue.setMaxtime);
+        Debug.Log("토탈 타이머: " + totalTime + " 현재 라운드: " + GameValue.Round + " 최대 라운드: " + GameValue.MaxRound + " 세팅 시간: " + GameValue.setMaxtime);
     }
 
     void Update()
@@ -44,34 +52,70 @@ public class GameTimer : MonoBehaviourPunCallbacks
             timerImage.localPosition = new Vector3(posX, timerImage.localPosition.y, timerImage.localPosition.z);
             timerImageFill_LT.fillAmount = ratio;
 
-            // 타이머 텍스트 업데이트
-            int minutes = Mathf.FloorToInt(currentTime / 60f);
-            int seconds = Mathf.FloorToInt(currentTime % 60f);
-        }
+            // 타이머가 0 이하로 떨어지면 이미지 변경 처리
+            if (currentTime < 0)
+            {
+                HandleTimeChange();
+            }
 
-        // f1 키를 누르면 시간 감소
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            photonView.RPC("RPC_DecreaseTime", RpcTarget.AllBuffered);
+            // f1 키를 누르면 시간 감소
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                photonView.RPC("RPC_DecreaseTime", RpcTarget.AllBuffered);
+            }
         }
     }
+
     [PunRPC]
     void RPC_All_Inside_Time()
     {
         GameValue.insideUser++;
-        if(GameValue.insideUser >= GameValue.MaxUser)
+        if (GameValue.insideUser >= GameValue.MaxUser)
         {
             currentTime = 0;
+            HandleTimeChange(); // Ensure time change handling if insideUser reaches max
         }
     }
+
     [PunRPC]
     void RPC_DecreaseTime()
     {
         currentTime -= decreaseTime;
-        // 타이머가 음수가 되지 않도록 보정
+
+        // 타이머가 0 이하로 떨어지면 이미지 변경 처리
         if (currentTime < 0)
         {
-            currentTime = totalTime;
+            HandleTimeChange();
+        }
+    }
+
+    void HandleTimeChange()
+    {
+        if (tempImage == null)
+        {
+            tempImage = new GameObject("TempImage").AddComponent<Image>();
+        }
+
+        switchingTime = !switchingTime;
+        currentTime = totalTime;
+
+        if (switchingTime)
+        {
+            // Swap sprites
+            tempImage.sprite = timerImageFill_LT.sprite;
+            timerImageFill_LT.sprite = timerlmageFill_HT.sprite;
+            timerlmageFill_HT.sprite = tempImage.sprite;
+            Wave_Time_Active_LT.SetActive(false);
+            Wave_Time_Active_HT.SetActive(true);
+        }
+        else
+        {
+            // Swap sprites
+            tempImage.sprite = timerlmageFill_HT.sprite;
+            timerlmageFill_HT.sprite = timerImageFill_LT.sprite;
+            timerImageFill_LT.sprite = tempImage.sprite;
+            Wave_Time_Active_LT.SetActive(true);
+            Wave_Time_Active_HT.SetActive(false);
         }
     }
 }
